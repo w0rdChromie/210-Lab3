07befbb706b600ea2b083bc8f62fa2bccb804d4d
w0rdChromie <clhumphrey0801@gmail.com>
2019-09-26 14:30:08 -0400

    Copied Project

---
+++ Assets/ballCont.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballCont : MonoBehaviour
+{
+    private Rigidbody2D rigidb2D;
+
+    [SerializeField]
+    private float pacSpeed = 1;
+
+    Vector2 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = new Vector2(transform.position.x, transform.position.y);
+        rigidb2D = GetComponent<Rigidbody2D>();
+        rigidb2D.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            transform.position = new Vector3(transform.position.x + pacSpeed, transform.position.y, transform.position.z);
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            transform.position = new Vector3(transform.position.x - pacSpeed, transform.position.y, transform.position.z);
+        }
+
+        if(Input.GetKey("space"))
+        {
+            rigidb2D.isKinematic = false;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.position = startPos;
+        }
+    }
+}
---
+++ Assets/win.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class win : MonoBehaviour
+{
+    [SerializeField] private string lev;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("player"))
+        {
+            SceneManager.LoadScene(lev);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
